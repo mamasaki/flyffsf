ca80e00a29eed7e244ff24fbff96293d0289c718
66430417@qq.com <66430417@qq.com@e2c90bd7-ee55-cca0-76d2-bbf4e3699278>
2010-12-23 15:34:08 +0000

    告白修改

--- trunk/SRC/OFFI SRC!/_Interface/WndIndirectTalk.h
+++ trunk/SRC/OFFI SRC!/_Interface/WndIndirectTalk.h
@@ -5,7 +5,7 @@ class CWndIndirectTalk : public CWndNeuz
 { 
 public: 
 	CWndIndirectTalk(); 
-	~CWndIndirectTalk(); 
+	virtual ~CWndIndirectTalk(); 
 
 	virtual BOOL Initialize( CWndBase* pWndParent = NULL, DWORD nType = MB_OK ); 
 	virtual BOOL OnChildNotify( UINT message, UINT nID, LRESULT* pLResult ); 
@@ -16,4 +16,12 @@ public:
 	virtual void OnLButtonUp( UINT nFlags, CPoint point ); 
 	virtual void OnLButtonDown( UINT nFlags, CPoint point ); 
 }; 
+
+class CWndCoupleTalk:public CWndIndirectTalk
+{
+public: 
+	virtual BOOL Initialize( CWndBase* pWndParent = NULL, DWORD nType = MB_OK ); 
+	virtual BOOL OnChildNotify( UINT message, UINT nID, LRESULT* pLResult ); 
+	virtual void OnDraw( C2DRender* p2DRender ); 
+};
 #endif
